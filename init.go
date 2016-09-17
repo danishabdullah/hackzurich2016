ee96752614870a3661a5ffc79bc0a02ce30aa358
Robert Jacob <xperimental@solidproject.de>
2016-09-17 06:00:05 +0200

    Seed PRNG.

--- init.go
+++ init.go
@@ -2,10 +2,14 @@ package getrational
 
 import (
 	"log"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
 func init() {
+	rand.Seed(time.Now().UnixNano())
+
 	templ, err := loadTemplates()
 	if err != nil {
 		log.Fatalf("Can not load templates: %s", err)
